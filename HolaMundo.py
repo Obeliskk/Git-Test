64e276131c14f8a5f6edd89b3924d3ef383f4c4a
Diego Martinez <diegomn140@gmail.com>
2026-09-05 01:55:26 -0400

    Agrego Comentarios de uso

--- HolaMundo.py
+++ HolaMundo.py
@@ -4,3 +4,9 @@ print("Pero, si aun no existe Leslos...")
 print ("Tal vez, si existe Leslos, pero no lo conozco")
 #-------------------
 print("Intentare hacer una Rama 1")
+#Para crear una rama es usando git checkout -b "Nombre de tu rama" Escribele Rama2 ya que yo estoy en la Rama1
+#desde aqui todo es igual, modificas el archivo
+#Guardas el Archivo
+#Haces Git add nombre del archivo
+#Pero aqui cambia, ya que cuando es la primera vez que haces el push a remoto tienes que agregar
+#git push -u origin "Nombre de la Rama"
